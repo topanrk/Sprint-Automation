ed5b22df0a4399274b4c3b5c97ea9e5268935b5b
topanrk <47352748+topanrk@users.noreply.github.com>
2021-08-05 18:51:52 +0700

    Add files via upload

---
+++ latihan1.js
@@ -0,0 +1,31 @@
+/*alert('Selamat Datang di kelas ini....')
+var nama = prompt('Masukan nama anda : ')*/
+//document.write('Selamat datang topan' +'<br>')
+//document.write('Silahkan belajar dengan penuh semangat..!!' +'<br>')
+// var bil1 = parseInt(prompt('Masukan bilangan pertama '))
+// var bil2 = parseInt(prompt('Masukan bilangan kedua '))
+// var bil1 = 10
+// var bil2 = 20
+// var bil3 = bil1 + bil2
+// document.write('bilangan Pertama ' + bil1 + '<br>')
+// document.write('bilangan kedua ' + bil2 + '<br>')
+// document.write(bil1 < bil2)
+// document.write('<br>')
+// document.write(bil1 > bil2)
+// document.write('<br>')
+// document.write(bil1 < bil2 && bil1 > bil2)
+// var nilai = prompt('Masukkan nilai anda : ')
+// var kelulusan = (nilai > 80) ? 'lulus' : 'ulang'
+// document.write(kelulusan)
+
+//var Konfirmasi = confirm('Apakah kamu yakin?')
+//document.write(Konfirmasi + '<br>')
+// alert('test');
+// var bil1 = parseInt(prompt('Masukkan Bilangan Pertama '))
+// var bil2 = parseInt(prompt('Masukan Bilangan Kedua '))
+// var bil3 = bil1 + bil2
+// document.write('Hasilnya adalah ' + bil3 )
+alert ('Cek Kelulusan')
+var nilai 		= prompt('Masukkan Nilai Anda : ')
+var kelulusan = (nilai > 85)? 'Anda Lulus' : 'Anda mengulang'
+document.write('Anda : ' + kelulusan)
